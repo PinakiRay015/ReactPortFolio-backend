c554c9041a417ecd19ec0a890f6b33d0076ac92d
RohanRay015 <pinaki.bpd2001@gmail.com>
2023-06-11 10:59:52 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv');
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+const hostname = '127.0.0.1';
+
+app.get("/" , (req , res) =>
+{
+    res.send("welcome developers")
+})
+
+mongoose.connect(process.env.DATABASE).then(() =>
+{
+    try {
+        app.listen(port , hostname , () =>
+        {
+            console.log(`The server is listening on port ${port}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
